217a0c8eed73c2bacf58c2eb818c0f0cdfa0d0f4
alexisrr11 <alexis.r4995@gmail.com>
2025-07-01 18:02:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const lista = document.getElementById('lista-turnos');
 const nombre = localStorage.getItem("usuarioLogueado");
 
 if (!nombre) {
-  window.location.href = "./login.html";
+  window.location.href = "../login.html";
 }
 document.getElementById("nombre-mostrado").textContent = `Nombre: ${nombre}`;
 
